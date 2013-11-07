271aa7663ddd7b28469c7c10fda97d286f136667
dries007 <admin@dries007.net>
2013-11-07 01:35:51 +0100

    BAM, even more code!

---
+++ README.md
@@ -0,0 +1,33 @@
+# HoloInventory
+
+Adds Holographic inventory screen for all blocks with an inventory.
+
+WIP, no release yet. You can build with gradle.
+
+(c) Copyright  Dries007.net 2013
+
+## Setup dev env:
+
+- Install gradle
+- Run "gradle setupDevWorkspace" in the git repo (some steps seem to take forever, namely getAssets)
+- Import folder with Intellij or Eclipse
+- Make changes
+- Use the run configurations below
+- Run "gradle build" to make a jar, see "build/libs" for output
+- Test your changes
+- Make a PR!
+
+## Intellij run configurations:
+Don't forget to make "minecraft/jars".
+
+### Client:
+- net.minecraft.launchwrapper.Launch
+- -Djava.library.path=../natives -XX:-UseSplitVerifier
+- --version 1.6 --tweakClass cpw.mods.fml.common.launcher.FMLTweaker --username Dries007
+- $MODULE_DIR$/minecraft/jars
+
+### Server:
+- cpw.mods.fml.relauncher.ServerLaunchWrapper
+- -XX:-UseSplitVerifier
+- (empty)
+- $MODULE_DIR$/minecraft/jars
---
+++ src/main/java/net/dries007/holoInventory/lib/developercapes/DevCapesImageBufferDownload.java
@@ -0,0 +1,40 @@
+/**
+ * DeveloperCapes by Jadar
+ * License: MIT License (https://raw.github.com/jadar/DeveloperCapes/master/LICENSE)
+ * version 2.1
+ */
+package net.dries007.holoInventory.lib.developercapes;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+
+import net.minecraft.client.renderer.IImageBuffer;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class DevCapesImageBufferDownload implements IImageBuffer {
+	
+	private int imageWidth;
+	private int imageHeight;
+	
+	@Override
+	public BufferedImage parseUserSkin(BufferedImage par1BufferedImage) {
+		if (par1BufferedImage == null) {
+			return null;
+		}
+		else {
+			this.imageWidth = (par1BufferedImage.getWidth((ImageObserver)null) <= 64) ? 64: (par1BufferedImage.getWidth((ImageObserver)null));
+			this.imageHeight = (par1BufferedImage.getHeight((ImageObserver)null) <= 32) ? 32: (par1BufferedImage.getHeight((ImageObserver)null));
+			
+			BufferedImage capeImage = new BufferedImage(this.imageWidth, this.imageHeight, 2);
+			
+			Graphics graphics = capeImage.getGraphics();
+			graphics.drawImage(par1BufferedImage, 0, 0, (ImageObserver)null);
+			graphics.dispose();
+			
+			return capeImage;
+		}
+	}
+}
