7729293d2a3b0f67ebd9e0a2242af8334e68643f
Jamal <Jamalg960@gmail.com>
2016-09-30 21:22:59 -0400

    didn't mean to remove this

--- src/main/java/net/dries007/holoInventory/HoloInventory.java
+++ src/main/java/net/dries007/holoInventory/HoloInventory.java
@@ -44,7 +44,7 @@ import org.apache.logging.log4j.Logger;
 
 import static net.dries007.holoInventory.HoloInventory.*;
 
-@Mod(modid = MODID, name = MODID, acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[1.8.9]", canBeDeactivated = true, guiFactory = GUI_FACTORY)
+@Mod(modid = MODID, name = MODID, acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[1.8.9]", canBeDeactivated = true, updateJSON = URL + "update.json", guiFactory = GUI_FACTORY)
 public class HoloInventory
 {
     public static final String MODID = "HoloInventory";
